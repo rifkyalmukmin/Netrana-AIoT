2477952471fe20270ba4459fa77dcd2eb4e5c51f
rifkyalmukmin <rifky.10124706@mahasiswa.unikom.ac.id>
2025-09-07 00:04:50 +0700

    Refactor MotorControl class: consolidate multiple methods

--- lib/MotorControl/ MotorControl.cpp
+++ lib/MotorControl/ MotorControl.cpp
@@ -1,19 +1,21 @@
 #include "MotorControl.h"
 
-MotorControl::MotorControl(int motorPin) {
-    _motorPin = motorPin;
-    pinMode(_motorPin, OUTPUT);
-    digitalWrite(_motorPin, LOW);
+void MotorControl::begin(uint8_t pin, uint8_t channel, uint32_t freq, uint8_t resolution) {
+    chan_ = channel;
+    ledcSetup(channel, freq, resolution);
+    ledcAttachPin(pin, channel);
+    setDuty(0);
 }
 
-void MotorControl::vibrate(int durationMs) {
-    digitalWrite(_motorPin, HIGH);
-    delay(durationMs);
-    digitalWrite(_motorPin, LOW);
+void MotorControl::setDuty(uint8_t duty) {
+    ledcWrite(chan_, duty);
 }
 
-void MotorControl::stop() {
-    digitalWrite(_motorPin, LOW);
+void MotorControl::pulse(uint8_t duty, uint16_t on_ms, uint16_t off_ms, uint8_t repeat) {
+    for (uint8_t i = 0; i < repeat; i++) {
+        setDuty(duty);
+        vTaskDelay(pdMS_TO_TICKS(on_ms));
+        setDuty(0);
+        vTaskDelay(pdMS_TO_TICKS(off_ms));
+    }
 }
-
-
--- lib/MotorControl/MotorControl.h
+++ lib/MotorControl/MotorControl.h
@@ -1,16 +1,11 @@
-#ifndef MOTORCONTROL_H
-#define MOTORCONTROL_H
-
+#pragma once
 #include <Arduino.h>
 
 class MotorControl {
     public:
-        MotorControl(int motorPin);
-        void vibrate(int durationMs);
-        void stop();
-
-private:
- int _motorPin;
-
+    void begin(uint8_t pin, uint8_t channel = 0, uint32_t freq = 2000, uint8_t resolution = 8);
+    void setDuty(uint8_t duty); // 0..255
+    void pulse(uint8_t duty, uint16_t on_ms, uint16_t off_ms, uint8_t repeat);
+    private:
+    uint8_t chan_ = 0;
 };
-#endif 
