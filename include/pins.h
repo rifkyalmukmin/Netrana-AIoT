be86e788030ba0ef007ca385906bccba4bbf3799
rifkyalmukmin <rifky.10124706@mahasiswa.unikom.ac.id>
2025-10-01 22:34:28 +0700

    Refactor project structure: remove unused files, update settings, and enhance configuration

--- include/pins.h
+++ include/pins.h
@@ -1,48 +1,24 @@
 #pragma once
-#include <Arduino.h>
 
+// Camera pins
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM     5
+#define Y9_GPIO_NUM       4
+#define Y8_GPIO_NUM       6
+#define Y7_GPIO_NUM       7
+#define Y6_GPIO_NUM      14
+#define Y5_GPIO_NUM      17
+#define Y4_GPIO_NUM      21
+#define Y3_GPIO_NUM      18
+#define Y2_GPIO_NUM      16
+#define VSYNC_GPIO_NUM    1
+#define HREF_GPIO_NUM     2
+#define PCLK_GPIO_NUM    15
+#define SIOD_GPIO_NUM     8
+#define SIOC_GPIO_NUM     9
+#define LED_GPIO_NUM     47
 
-// ======================
-// Pinout — SESUAIKAN!
-// ======================
-// Dua sensor ultrasonik JSN‑SR04 (Trigger/Echo)
-// Sensor BAWAH (deteksi lubang/halangan bawah tongkat)
-#define US1_TRIG 8
-#define  US1 ECHO 9
-// Sensor ATAS TENGAH (deteksi tiang/halangan depan setinggi badan)
-#define US2_TRIG 10
-#define US2_ECHO 11
-
-// Vibration motor (via transistor + diode). Gunakan PWM (LEDC)
-#define VIBRATION_PIN 5
-
-// Tombol - Gunakan INPUT_PULLUP (aktif LOW)
-#define BUTTON_PANIC 2
-#define BUTTON_NAV_UP 3
-#define BUTTON_NAV_DOWN 4
-#define BUTTON_NAV_LEFT 6
-#define BUTTON_NAV_RIGHT 7
-
-// ==== CAM PIN MAP (contoh template; PERIKSA modul DFRobot Anda) ====
-// Jika memakai pin preset dari modul tertentu, ganti sesuai dokumen resmi.
-// Di bawah ini contoh pola umum; TIDAK menjamin cocok out‑of‑the‑box.
-
-#define CAM_PIN_PWDN -1 // Power down (gunakan -1 jika tidak ada)
-#define CAM_PIN_RESET -1 // Reset (gunakan -1 jika tidak ada)
-#define CAM_PIN_XCLK 15
-#define CAM_PIN_SIOD 17
-#define CAM_PIN_SIOC 18
-#define CAM_PIN_D7 16
-#define CAM_PIN_D6 37
-#define CAM_PIN_D5 14
-#define CAM_PIN_D4 13
-#define CAM_PIN_D3 12
-#define CAM_PIN_D2 11
-#define CAM_PIN_D1 48
-#define CAM_PIN_D0 47
-#define CAM_PIN_VSYNC 38
-#define CAM_PIN_HREF 40
-#define CAM_PIN_PCLK 39
-
-//XCLK frequency
-#define CAM_XCLK_FREQ 20000000 // 20 MHz umum untuk S3 + PSRAM
+// UART
+#define RX_PIN 44
+#define TX_PIN 43
--- include/secrets.h
+++ include/secrets.h
@@ -1,14 +1,3 @@
-//Wifi
-static const char* Wifi_SSID = "your-ssid";
-static const char* WIFI_PASSS = "your-password";
-
-//MQTT
-#define MQQT_BROKER "your-mqtt-broker"
-#define MQQT_PORT 1883;
-#define MQQT_USER "your-mqtt-user"
-#define MQQT_PASS "your-mqtt-password"
-
-//URL YOLO SERVER
-#define YOLO_URL "http://your-yolo-server/detect"
-
-
+#pragma once
+#define WIFI_SSID "Mangga ditampi"
+#define WIFI_PASSWORD "Blade#12"
